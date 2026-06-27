c134f14e0d652a61d075302ee2c16cd33dcddfd2
ashrifak <ashrifaash06@gmail.com>
2026-06-27 14:15:47 +0530

    test git

--- schemas/job.py
+++ schemas/job.py
@@ -1,11 +1,23 @@
 from pydantic import BaseModel
 from typing import Optional
 
-class JobCreate(BaseModel):
+class JobBase(BaseModel):
     title: str
-    lsalary: int
+    salary: int
+    description: Optional[str]=None
+    company_id: int
+
+class JobCreate(JobBase):
+    pass
 
 class JobUpdate(BaseModel):
     title: Optional[str] =None
     salary: Optional[str] =None
-    
+    description: Optional[str] =None
+    company_id: Optional[str] =None
+class JobResponse(JobBase):
+    id:int
+    company_id:int
+
+    class config:
+        from_attributes=True
