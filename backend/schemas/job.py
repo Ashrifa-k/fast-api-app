dc5b5d645ecf8957bb04051e29b3b5139cf3d844
ashrifak <ashrifaash06@gmail.com>
2026-07-01 13:56:58 +0530

    test git

---
+++ backend/schemas/job.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class JobBase(BaseModel):
+    title: str
+    salary: int
+    description: Optional[str]=None
+    company_id: int
+
+class JobCreate(JobBase):
+    pass
+
+class JobUpdate(BaseModel):
+    title: Optional[str] =None
+    salary: Optional[int] =None
+    description: Optional[str] =None
+    company_id: Optional[str] =None
+class JobResponse(JobBase):
+    id:int
+    company_id:int
+
+    class config:
+        from_attributes=True
